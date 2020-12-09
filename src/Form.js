26db5191d2e34ec366762c0ffb67b61e6e8547ef
cnitz16 <chelseanitzken@gmail.com>
2020-12-09 09:25:46 -0500

    Updated form to include columns

--- src/Form.js
+++ src/Form.js
@@ -4,14 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Form() {
   return (
     <form id="discover-search" className="form-inline d-flex">
-      <input
-        type="search"
-        className="form-control"
-        placeholder="Type in city..."
-        id="search-city"
-        autoComplete="off"
-      />
-      <input type="submit" className="btn btn-dark" value="Discover" />
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            className="form-control"
+            placeholder="Type in city..."
+            id="search-city"
+            autoComplete="off"
+          />
+        </div>
+        <div className="col-3">
+          <input type="submit" className="btn btn-dark" value="Discover" />
+        </div>
+      </div>
     </form>
   );
 }
