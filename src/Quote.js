77a8e030dc02c51ab2ac61624e5e71b0201b2b12
cnitz16 <chelseanitzken@gmail.com>
2020-12-09 09:15:55 -0500

    added css to quote and center aligned it

--- src/Quote.js
+++ src/Quote.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./quote.css";
 
 export default function Quote() {
   return (
-    <h5>
+    <h5 className="quote">
       "Climate is what we expect, weather is what we get."
       <br />
       -Mark Twain
