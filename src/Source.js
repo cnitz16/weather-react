ee490cb9b3e6da53c71f6a1955a44b016c00c95a
cnitz16 <chelseanitzken@gmail.com>
2020-12-16 13:08:35 -0500

    added link to netlify

--- src/Source.js
+++ src/Source.js
@@ -4,7 +4,8 @@ export default function Source() {
   return (
     <span className="github">
       <a href="https://github.com/cnitz16/weather-react">Open-source Code</a> by
-      Chelsea Nitzken
+      Chelsea Nitzken and hosted on{" "}
+      <a href="https://weather-app-react-cnitz.netlify.app/">Netlify</a>
     </span>
   );
 }
